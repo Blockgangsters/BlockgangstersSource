03bb4a98251c75b1702253321e436d257a7fd3d9
BlockGangHenk <blockchainfan11@gmail.com>
2021-10-31 12:34:36 +0100

    AdminPage

---
+++ src/pages/Admin/Admin.elements.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+//TokenomicsContainer is around whole page, sets background etc.
+export const PageWrapper = styled.div`
+max-width: 100%;
+max-height: 100%;
+padding: 0 0; // no padding, thats in the container
+background: rgba(16,21,34,.9);
+margin-left: 20vw;
+`
+
+//TokenomicsContainer is around whole page, sets background etc.
+export const CrimeContainer = styled.div`
+max-width: 70%;
+max-height: 100%;
+padding: 30px 40px;
+justify-content: space-around;
+flex-direction: row; // this is standard, fit from left to right
+display: flex;
+color: white;
+
+@media screen and (max-width: 768px) {
+    padding-bottom: 65px;
+};
+`
+
+//TokenomicsContainer is around whole page, sets background etc.
+export const Title = styled.h1`
+max-width: 100%;
+max-height: 10%;
+
+padding: 160px 40px 0px 40px;
+justify-content: space-around;
+flex-direction: row; // this is standard, fit from left to right
+display: relative;
+color: white;
+vertical-align: text-top;
+
+@media screen and (max-width: 768px) {
+    padding-bottom: 65px;
+};
+`
+//TokenomicsContainer is around whole page, sets background etc.
+export const SubTitle = styled.h4`
+max-width: 100%;
+max-height: 10%;
+
+padding: 20px 40px 0px 40px;
+justify-content: space-around;
+flex-direction: row; // this is standard, fit from left to right
+display: relative;
+color: white;
+vertical-align: text-top;
+
+@media screen and (max-width: 768px) {
+    padding-bottom: 65px;
+};
+`
---
+++ src/pages/Admin/Admin.js
@@ -0,0 +1,74 @@
+import {PageWrapper, CrimeContainer, Title, SubTitle} from './Admin.elements';
+import React, {useEffect }  from 'react';
+import {tokenAddress, getLINKBalance, transferETH } from '../../components/EthFunctions';
+import {ethers} from 'ethers';
+import NumberFormat from "react-number-format";
+import {StateContext} from '../../App';
+import {ColoredLine, SubmitButton} from '../../globalStyles'
+
+
+
+const Admin = () => {
+
+    const [, , mmConnected, , , ] = React.useContext(StateContext);
+    const [linkBalance, setLinkBalance] = React.useState(0);
+    const [ethBalance, setEthBalance] = React.useState(0);
+
+    useEffect(() => {  
+      const fetchEvents = async() => {
+        console.log("mm conn: ", mmConnected)
+        if (mmConnected) {
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const balance = await provider.getBalance(tokenAddress);
+          //const balanceToEth = balance.div(10**18);
+          console.log("MATIC in contract: ", (ethers.utils.formatEther( balance )))
+          setEthBalance(ethers.utils.formatEther( balance ));
+          const linkBalance = await getLINKBalance();
+          setLinkBalance(linkBalance);
+        }
+      }
+        fetchEvents();
+    }, [mmConnected]); // trigger on setTriggerEvents if we want to update every 20s
+
+    //event tokensDeposited(address indexed sender, uint256 indexed amount);
+    //event tokensWithdrawn(address indexed sender, uint256 indexed amount);
+
+    return (
+        <PageWrapper>
+          <Title> Admin page </Title><br />
+          <SubTitle> Available LINK in contract: <NumberFormat
+            value={linkBalance}
+            decimalSeparator="."
+            displayType="text"
+            decimalScale={3}
+            type="text" />  ( or <NumberFormat
+                value={linkBalance/0.0001 }
+                decimalSeparator="."
+                displayType="text"
+                decimalScale={4}
+                type="text" /> actions)     
+            </SubTitle>
+            <SubTitle> Available MATIC in contract: <NumberFormat
+            value={ethBalance}
+            decimalSeparator="."
+            displayType="text"
+            decimalScale={3}
+            type="text" />   
+            <SubmitButton onClick={() => { transferETH()}}> Withdraw! </SubmitButton> </SubTitle> 
+
+            
+
+            <CrimeContainer>
+
+
+            
+
+            </CrimeContainer>
+            <ColoredLine color="red" />
+
+        </PageWrapper>
+        
+    )
+}
+
+export default Admin
