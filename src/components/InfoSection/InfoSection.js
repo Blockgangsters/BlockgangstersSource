3a25f162a0fc437fcc6144dd5cfd85a8d82f26e0
Tico Raaphorst <>
2021-11-05 12:01:19 +0100

    feat(CountdownTimer): Add a custom countdown timer component

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { TitleWrapper, ReleaseWrapper, StyledInfoSection, InfoRow, InfoColumn, TextWrapper, TopLine, Heading, Subtitle, CircleWrapper, TitleBox, TimerText, SocialIcons, SocialIconLink, BootstrapText, BootstrapWrapper } from './InfoSection.elements';
+import { StyledInfoSection, InfoRow, InfoColumn, TextWrapper, TopLine, Heading, Subtitle, TimerText, SocialIcons, SocialIconLink, BootstrapText, BootstrapWrapper } from './InfoSection.elements';
 import { Button } from '../../features/shared/ui/buttons/Button';
 import { Link } from 'react-router-dom'
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import { FaTelegram, FaTwitter, FaDiscord } from 'react-icons/fa';
 import { StateContext, EthContext } from '../../App';
 import { connectWallet } from '../EthFunctions'
@@ -10,43 +9,15 @@ import { connectWallet } from '../EthFunctions'
 import AnimatedNumbers from "react-animated-numbers";
 import styled from '@emotion/styled/macro';
 import { breakpoint } from '../../styles/theme/responsive/breakpoint';
-
-const minuteSeconds = 60;
-const hourSeconds = 3600;
-const daySeconds = 86400;
+import { CountdowndTimer } from '../../features/countdowntimer/CountdownTimer';
 
 
-const timerProps = {
-    isPlaying: true,
-    size: 80,
-    strokeWidth: 6
-};
 
-const renderTime = (dimension, time) => {
-    return (
-        <div className="time-wrapper">
-            <div className="time">{time}</div>
-            <div>{dimension}</div>
-        </div>
-    );
-};
-
-const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
-const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
-const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
-const getTimeDays = (time) => (time / daySeconds) | 0;
 
 
 
 const InfoSection = ({ countdownTimer, bootstrapTimer, primary, lightBg, imgStart, lightTopLine, lightText, lightTextDesc, buttonLabel, description, headline, topLine, img, alt, start }) => {
-    // start CountdownCircle  
-    const stratTime = Date.now() / 1000; // 23 10 21
-    const endTime = 1637017200; // 1 nov 21
-    const remainingTime = endTime - stratTime;
-    const days = Math.ceil(remainingTime / daySeconds);
-    const daysDuration = days * daySeconds;
 
-    // start react-animated-numbers
     const [, , mmConnected, , ,] = React.useContext(StateContext);
     const [, , , , , , , , , , , , , , , , , , bootstrapUsed] = React.useContext(EthContext);
 
@@ -56,107 +27,51 @@ const InfoSection = ({ countdownTimer, bootstrapTimer, primary, lightBg, imgStar
                 <InfoRow imgStart={imgStart}>
                     <InfoColumn>
                         <TextWrapper>
-
-
-                            {countdownTimer === true ? <ReleaseWrapper>
-                                <TitleWrapper to="/trainstats"> Release Candidate live on Mumbai testnet. Click here to start playing now!</TitleWrapper> <TitleBox> Main net release in: </TitleBox> <CircleWrapper> <CountdownCircleTimer
-                                    {...timerProps}
-                                    colors={[["#a9b3c1"]]}
-                                    strokeWidth="12"
-                                    trailColor="#101522"
-                                    duration={daysDuration}
-                                    initialRemainingTime={remainingTime}
-                                >
-                                    {({ elapsedTime }) =>
-                                        renderTime("days", getTimeDays(daysDuration - elapsedTime))
-                                    }
-                                </CountdownCircleTimer>
-                                    <CountdownCircleTimer
-                                        {...timerProps}
-                                        colors={[["#a9b3c1"]]}
-                                        strokeWidth="12"
-                                        duration={daySeconds}
-                                        trailColor="#101522"
-                                        initialRemainingTime={remainingTime % daySeconds}
-                                        onComplete={(totalElapsedTime) => [
-                                            remainingTime - totalElapsedTime > hourSeconds
-                                        ]}
-                                    >
-                                        {({ elapsedTime }) =>
-                                            renderTime("hours", getTimeHours(daySeconds - elapsedTime))
-                                        }
-                                    </CountdownCircleTimer>
-                                    <CountdownCircleTimer
-                                        {...timerProps}
-                                        colors={[["#a9b3c1"]]}
-                                        strokeWidth="12"
-                                        duration={hourSeconds}
-                                        trailColor="#101522"
-                                        initialRemainingTime={remainingTime % hourSeconds}
-                                        onComplete={(totalElapsedTime) => [
-                                            remainingTime - totalElapsedTime > minuteSeconds
-                                        ]}
-                                    >
-                                        {({ elapsedTime }) =>
-                                            renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
-                                        }
-                                    </CountdownCircleTimer>
-                                    <CountdownCircleTimer
-                                        {...timerProps}
-                                        colors={[["#a9b3c1"]]}
-                                        duration={minuteSeconds}
-                                        strokeWidth="12"
-                                        trailColor="#101522"
-                                        initialRemainingTime={remainingTime % minuteSeconds}
-                                        onComplete={(totalElapsedTime) => [
-                                            remainingTime - totalElapsedTime > 0
-                                        ]}
-                                    >
-                                        {({ elapsedTime }) =>
-                                            renderTime("seconds", getTimeSeconds(elapsedTime))
-                                        }
-                                    </CountdownCircleTimer>
-                                </CircleWrapper>
-                            </ReleaseWrapper> : null}
-                            {countdownTimer ? <> <TimerText> Join us now on social media to get started right away! </TimerText>
-                                <SocialIcons>
-                                    <SocialIconLink href="https://discord.gg/j3JGYthB" target="_blank" aria-label="Discord">
-                                        <FaDiscord />
-                                    </SocialIconLink>
-                                    <SocialIconLink href="https://twitter.com/blockgangsters" target="_blank" aria-label="Twitter">
-                                        <FaTwitter />
-                                    </SocialIconLink>
-                                    <SocialIconLink href="https://t.me/joinchat/hIJ1FYLDul1jNGQ0" target="_blank" aria-label="Telegram">
-                                        <FaTelegram />
-                                    </SocialIconLink>
-                                </SocialIcons>
-
-
-                            </>
-                                : <> <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                            {countdownTimer && <CountdowndTimer countdownDate={new Date('2021-11-15')} />}
+                            {countdownTimer &&
+                                <>
+                                    <TimerText> Join us now on social media to get started right away! </TimerText>
+                                    <SocialIcons>
+                                        <SocialIconLink href="https://discord.gg/j3JGYthB" target="_blank" aria-label="Discord">
+                                            <FaDiscord />
+                                        </SocialIconLink>
+                                        <SocialIconLink href="https://twitter.com/blockgangsters" target="_blank" aria-label="Twitter">
+                                            <FaTwitter />
+                                        </SocialIconLink>
+                                        <SocialIconLink href="https://t.me/joinchat/hIJ1FYLDul1jNGQ0" target="_blank" aria-label="Telegram">
+                                            <FaTelegram />
+                                        </SocialIconLink>
+                                    </SocialIcons>
+                                </>}
+                            {!countdownTimer &&
+                                <>
+                                    <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
                                     <Heading lightText={lightText}>{headline}</Heading>
                                     <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
                                     <Link to='/sign-up'>
                                         {mmConnected ? null : <Button big fontBig onClick={connectWallet} primary={primary}>{buttonLabel}</Button>}
-                                    </Link> </>}
-                            {bootstrapTimer ? <>
-                                <BootstrapWrapper>
-                                    <AnimatedNumbers
-                                        includeComma
-                                        animateToNumber={bootstrapUsed}
-                                        fontStyle={{ fontSize: 20 }}
-                                        configs={[
-                                            { mass: 1, tension: 220, friction: 100 },
-                                            { mass: 1, tension: 180, friction: 130 },
-                                            { mass: 1, tension: 280, friction: 90 },
-                                            { mass: 1, tension: 180, friction: 135 },
-                                            { mass: 1, tension: 260, friction: 100 },
-                                            { mass: 1, tension: 210, friction: 1000 },
-                                        ]}
-                                    />
-                                    <BootstrapText> /1,000 bootstrap slots used </BootstrapText>
-                                </BootstrapWrapper>
-                            </> : null}
+                                    </Link>
+                                </>}
+                            {bootstrapTimer &&
+                                <>
+                                    <BootstrapWrapper>
+                                        <AnimatedNumbers
+                                            includeComma
+                                            animateToNumber={bootstrapUsed}
+                                            fontStyle={{ fontSize: 20 }}
+                                            configs={[
+                                                { mass: 1, tension: 220, friction: 100 },
+                                                { mass: 1, tension: 180, friction: 130 },
+                                                { mass: 1, tension: 280, friction: 90 },
+                                                { mass: 1, tension: 180, friction: 135 },
+                                                { mass: 1, tension: 260, friction: 100 },
+                                                { mass: 1, tension: 210, friction: 1000 },
+                                            ]}
+                                        />
+                                        <BootstrapText> /1,000 bootstrap slots used </BootstrapText>
+                                    </BootstrapWrapper>
+                                </>
+                            }
                         </TextWrapper>
                     </InfoColumn>
 
---
+++ src/features/countdowntimer/CountdownTimer.tsx
@@ -0,0 +1,103 @@
+import styled from "@emotion/styled/macro"
+import { useEffect, useState } from "react"
+
+
+interface ICountdownTimerProps {
+    countdownDate: Date;
+}
+
+export const CountdowndTimer = ({ countdownDate }: ICountdownTimerProps) => {
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+
+    const [days, setDays] = useState(100);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const countdown = countdownDate.getTime()
+            const now = new Date().getTime();
+            const distance = countdown - now;
+
+            setDays(Math.floor(distance / (day)));
+            setHours(Math.floor((distance % (day)) / (hour)));
+            setMinutes(Math.floor((distance % (hour)) / (minute)));
+            setSeconds(Math.floor((distance % (minute)) / second));
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, [countdownDate, day, hour, minute])
+
+
+    return (
+        <StyledCountdownTimer>
+            <StyledHeadline id="headline">Countdown to mainnet release</StyledHeadline>
+            <StyledCountdown>
+                <StyledCountdownList>
+                    <StyledCountdownElement>
+                        <StyledNumber>{days}</StyledNumber>
+                        <StyledText>days</StyledText>
+                    </StyledCountdownElement>
+                    <StyledCountdownElement>
+                        <StyledNumber>{hours}</StyledNumber>
+                        <StyledText>Hours</StyledText>
+                    </StyledCountdownElement>
+                    <StyledCountdownElement>
+                        <StyledNumber>{minutes}</StyledNumber>
+                        <StyledText>Minutes</StyledText>
+                    </StyledCountdownElement>
+                    <StyledCountdownElement>
+                        <StyledNumber>{seconds}</StyledNumber>
+                        <StyledText>Seconds</StyledText>
+                    </StyledCountdownElement>
+                </StyledCountdownList>
+            </StyledCountdown>
+        </StyledCountdownTimer>
+    )
+}
+
+const StyledCountdownTimer = styled.div`
+
+`
+
+const StyledCountdown = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const StyledCountdownList = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+const StyledHeadline = styled.h1`
+    font-weight: normal;
+    letter-spacing: .125rem;
+    text-transform: uppercase;
+`
+
+const StyledCountdownElement = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1em;
+    text-transform: uppercase;
+`
+
+const StyledNumber = styled.span`
+    display: block;
+    font-size: 4.5rem;
+`
+
+const StyledText = styled.span`
+    display: block;
+    font-size: 1.5em;
+`
